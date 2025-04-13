eb709db9f87668af809edf6dd3244ae5a382a514
DParkk <pjymar28@gmail.com>
2025-04-12 21:35:10 -0700

    fixed yml files

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(dataVizModelR)
+library(rCreditCard)
 
-test_check("dataVizModelR")
+test_check("rCreditCard")
