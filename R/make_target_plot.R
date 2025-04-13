f506bac37426633c66c0a298e5c6efaa978b5edf
DParkk <99514339+DParkk@users.noreply.github.com>
2025-04-12 21:58:15 -0700

    Update make_target_plot.R

--- R/make_target_plot.R
+++ R/make_target_plot.R
@@ -2,6 +2,9 @@
 #'
 #' @param df A data frame containing the target column "default_payment_next_month"
 #' @return A ggplot object showing the bar plot
+#' @examples
+#' make_target_plot(df)  # df must include default_payment_next_month
+
 #' @export
 make_target_plot <- function(df) {
   if (!"default_payment_next_month" %in% colnames(df)) {
